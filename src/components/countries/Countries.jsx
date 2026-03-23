42c70a3797b18e6a3802de77b4241fea86a6c3ce
Mr-Xibon94 <md19jibon94@gmail.com>
2026-03-23 13:25:52 +0600

    added button and functionality

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -1,17 +1,25 @@
 import React, { use } from 'react';
 import Country from '../country/Country';
+import './Countries.css'
 
 const Countries = ({countriesPromise}) => {
     const countriesData = use(countriesPromise);
     const countries=countriesData.countries;
     // console.log(countries); 
-    return (
-        <div>
-            <h1>In the countries: {countries.length}</h1>
 
-            {
-                countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+    const visitedCountryHandler = (country) =>{
+        console.log(country);
+    }
+    return (
+        <div >
+            <h1>Total Countries: {countries.length}</h1>
+            <h4>Visited Countr: </h4>
+
+            <div className='countries'>
+                {
+                countries.map(country => <Country key={country.cca3.cca3} country={country} visitedCountryHandler ={visitedCountryHandler}></Country>)
             }
+            </div>
         </div>
     );
 };
--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Country.css'
 
-const Country = ({country}) => {
+const Country = ({country, visitedCountryHandler}) => {
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        setVisited(!visited);
+        visitedCountryHandler(country);
+    }
 
     // console.log(country.flags.flags); 
     return (
-        <div>
+        <div className={`country ${visited && 'countryVisited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h3>NAME: {country.name.common} </h3>
             <h4>Population: {country.population.population } </h4>
+            <h4>Area: {country.area.area} ({country.area.area > 300000? "Big Country":"Small Country"}) </h4>
+
+            <button onClick={handleVisited}  className='countryBtn'>{visited ? 'Visited':'Not Visited' }</button>
         </div>
+        
     );
 };
 
